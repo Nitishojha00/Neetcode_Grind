b500c103d58d5aa80306d9a43a8a62cacb2b6a69
Nitish Ojha <156918607+Nitishojha00@users.noreply.github.com>
2026-07-15 01:26:03 +0530

    Add: continuous-subarray-sum - submission-3

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        int sum = nums[0];
+        unordered_map<int,int>mp;
+        mp[0] = 0;
+        for(int i=1;i<n;i++)
+           {
+              sum+=nums[i];
+              if(mp.find(sum%k)!=mp.end())return true;
+              mp[sum%k] = i;
+           }
+           return false;
+    }
+};
