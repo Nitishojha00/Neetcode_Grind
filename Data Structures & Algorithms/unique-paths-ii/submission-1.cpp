a5e453eb63acef58ad479f2dad487e0d46418892
Nitish Ojha <156918607+Nitishojha00@users.noreply.github.com>
2026-08-31 13:52:25 +0530

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& a) {
+        
+        int n = a.size(), m = a[0].size();
+        vector dp(n+1,vector<int>(m+1,0));
+        dp[1][0] = 1;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(a[i-1][j-1]==1) continue;
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[n][m];
+
+    }
+};
